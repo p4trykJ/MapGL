0b5dcaeba3a0f1b90ede81d476afff8acf9cb3cd
Patryk <patryk.jezierski@gis-support.pl>
2019-01-03 01:59:22 +0100

    'bookmarks' - feature + 'popups' - feature + new layer

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Imports
 
-// Map
+// MAP
+// Map - instance
 mapboxgl.accessToken = 'pk.eyJ1IjoicGplemllcnNraTI0IiwiYSI6ImNqbXJ6NW04dTI1d3kzcHBkM3djbGwwMGoifQ.UlrhKR6v9F7glTdNQXRL1g';
 const map = new mapboxgl.Map({
 	container: 'map',
@@ -9,67 +10,265 @@ const map = new mapboxgl.Map({
 	zoom: 9.80908506743856
 });
 
+// Map - options
+
 var options = {
 	showCompass: true,
 	showZoom: true
 };
+
+// Map - features
+
 var scale = new mapboxgl.ScaleControl({
 	maxWidth: 200,
 	unit: 'metric'
 });
 
+// Map - addFeature
+
 map.addControl(scale);
 map.addControl(new mapboxgl.NavigationControl(options), 'top-right');
 
-// Bookmarks
+// Map - functions
+
+map.on('load', function () {
+
+	// Map - addLayer
+
+	map.addLayer({
+			"id": "symbole",
+			"type": "symbol",
+			"source": {
+				"type": "geojson",
+				"data": {
+					"type": "FeatureCollection",
+					"features": [{
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+							"icon": "theatre"
+						},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [16.5, 52.5]
+						}
+					}, {
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href=\"http://madmens5finale.eventbrite.com/\" target=\"_blank\" title=\"Opens in a new window\">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>",
+							"icon": "theatre"
+						},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [16.2, 52.2]
+						}
+					}, {
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a <a href=\"http://tallulaeatbar.ticketleap.com/2012beachblanket/\" target=\"_blank\" title=\"Opens in a new window\">Big Backyard Beach Bash and Wine Fest</a> on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>",
+							"icon": "bar"
+						},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [16.3, 52.3]
+						}
+					}, {
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Ballston Arts & Crafts Market</strong><p>The <a href=\"http://ballstonarts-craftsmarket.blogspot.com/\" target=\"_blank\" title=\"Opens in a new window\">Ballston Arts & Crafts Market</a> sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>",
+							"icon": "art-gallery"
+						},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [16.4, 52.4]
+						}
+					}, {
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Seersucker Bike Ride and Social</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's <a href=\"http://dandiesandquaintrelles.com/2012/04/the-seersucker-social-is-set-for-june-9th-save-the-date-and-start-planning-your-look/\" target=\"_blank\" title=\"Opens in a new window\">Seersucker Social</a> bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>",
+							"icon": "bicycle"
+						},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [16.9, 52.9]
+						}
+					}, {
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Capital Pride Parade</strong><p>The annual <a href=\"http://www.capitalpride.org/parade\" target=\"_blank\" title=\"Opens in a new window\">Capital Pride Parade</a> makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>",
+							"icon": "star"
+						},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [16.7, 52.7]
+						}
+					}, {
+						"type": "Feature",
+						"properties": {
+							"description": "<strong>Muhsinah</strong><p>Jazz-influenced hip hop artist <a href=\"http://www.muhsinah.com\" target=\"_blank\" title=\"Opens in a new window\">Muhsinah</a> plays the <a href=\"http://www.blackcatdc.com\">Black Cat</a> (1811 14th Street NW) tonight with <a href=\"http://www.exitclov.com\" target=\"_blank\" title=\"Opens in a new window\">Exit Clov</a> and <a href=\"http://godsilla.bandcamp.com\" target=\"_blank\" title=\"Opens in a new window\">Gods’illa</a>. 9:00 p.m. $12.</p>",
+							"icon": "music"
+						}
+					}]
+				}
+			},
+		"layout": {
+			"icon-image": "{icon}-15",
+			"icon-allow-overlap": true
+		}
+	});
+
+	//Map - popout
+
+	map.on('click', 'Flickr', function (e) {
+		new mapboxgl.Popup()
+		.setLngLat(e.lngLat)
+		.setHTML(e.features[0].properties.jpt_nazwa_)
+		.addTo(map);
+	});
+
+	map.on('click', 'symbole', function (e) {
+		var coordinates = e.features[0].geometry.coordinates.slice();
+		var description = e.features[0].properties.description;
+
+		// Ensure that if the map is zoomed out such that multiple
+		// copies of the feature are visible, the popup appears
+		// over the copy being pointed to.
+		while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+		}
+
+		new mapboxgl.Popup()
+		.setLngLat(coordinates)
+		.setHTML(description)
+		.addTo(map);
+	});
+
+	// Change the cursor to a pointer when the mouse is over the states layer.
+	map.on('mouseenter', 'states-layer', function () {
+		map.getCanvas().style.cursor = 'pointer';
+	});
+
+	// Change it back to a pointer when it leaves.
+	map.on('mouseleave', 'states-layer', function () {
+		map.getCanvas().style.cursor = '';
+	});
+});
+	
+// DATA
+
+// Data - bookmarks
 var bookmarks = [
 	{
 		name: "Zakładka1",
-		center: [66.5, 12]
+		center: {
+			lat: 61.81490583215361,
+			lng: 55.954416392710925
+		}
 	},
 	{
 		name: "Zakładka2",
-		center: [72.5, 12]
+		center: {
+			lat: 61.81490583215361,
+			lng: 55.954416392710925
+		}
 	},
 	{
 		name: "Zakładka3",
-		center: [72.5, 12]
+		center: {
+			lat: 61.81490583215361,
+			lng: 55.954416392710925
+		}
 	},
 	{
 		name: "Zakładka4",
-		center: [72.5, 12]
+		center: {
+			lat: 61.81490583215361,
+			lng: 55.954416392710925
+		}
 	}
 ]
 
 $(document).ready(flyTo = function() {
 	let selectBookmarks = $("#select")
 		selectBookmarks[0].onchange = function() {
-			let bookmarkData = $("#select").find(":selected").data("value")
+			let bookmarkData = $("#select").find(":selected").data().center
 			map.flyTo({
 				center: [
-					bookmarkData.center[0] + (Math.random() - 0.5) * 10,
-					bookmarkData.center[1] + (Math.random() - 0.5) * 10]
+					bookmarkData.lng,
+					bookmarkData.lat
+				]
 			})
 	}
 })
 
 $(document).ready(removeBookmark = function() {
-	$("#group__button--remove")[0].onclick = function() {
-		let removedBookmarkData = $("#select").find(":selected").data("value")
+	$("#bookmarks__button--remove")[0].onclick = function() {
+		let removedBookmarkData = $("#select").find(":selected").data()
 		for(let i = 0; i < bookmarks.length; i++) {
 			if(bookmarks[i].name === removedBookmarkData.name) {
 				$("#select").find(":selected").remove()
+				$('#select').val("seleceted")
 				bookmarks.splice(i, 1)
 			}
 		}
 	}
 })
 
-$(document).ready(buildBookmarkSelect = function() {
-	let firstOption = "<option id='firstSelected' disabled selected>Zakładki</option>"
-	$("#select").append(firstOption) 
-	bookmarks.forEach(bookmark => {
-		let option = "<option label='" + bookmark.name + "' data-value='{" + '"' + "name" + '"' + ":" + '"' + bookmark.name + '"' + ' ,"' + "center" +'"'+ ":[" + bookmark.center + "]}'></option>"
-		$("#select").append(option)
-	})
+$(document).ready(addBookmark = function() {
+	$("#bookmarks__button--add")[0].onclick = function() {
+		$('.modal').on('shown.bs.modal', function() {
+			$(this).find('[autofocus]').focus();
+		});
+		$("#bookmarks__button--save")[0].onclick = function() {
+			bookmark = {
+				name: $("#bookmark-name").val(),
+				center: map.getCenter()
+			}
+			bookmarks.push(bookmark)
+			buildBookmarkSelect()
+			$("#bookmark-name").val("")
+			$('.modal').modal('hide')
+		}
+	}
 })
+
+$(document).ready(buildBookmarkSelect = function() {
+	let select = $("#select")
+	select.empty()
+	let firstOption = "<option id='firstSelected' value='seleceted' disabled hidden selected>Zakładki</option>"
+	select.append(firstOption)
+	bookmarks.forEach(bookmark => {
+		let option = document.createElement("OPTION");
+		option.text = bookmark.name
+		jQuery.data(option, bookmark)
+		select.append(option);
+	})
+});
+
+
+
+
+/*
+$(document).ready(createSocialIcons = function() {
+	let socialIconsParent = $('div.mapboxgl-control-container').children(".mapboxgl-ctrl-bottom-left");
+	console.log("rodzic", socialIconsParent);
+	let socialIcons = document.createElement("div");
+	socialIcons.className = "socialIcons__wrapper";
+	var html = `
+		<a class="socialIcon__wrapper--github btn btn-social-icon btn-github">
+			<span class="socialIcons__icon fab fa-github"></span>
+		</a>
+	`;
+
+	console.log("przed", socialIcons)
+	$('a.socialIcon__wrapper--github').html(html)
+	console.log(html)
+	console.log("po",socialIcons)
+	let url = "https://github.com/p4trykJ";
+
+	$(".socialIcon__wrapper--github").attr("href", url).attr("target", "_blank")[0].click();
+	socialIconsParent.append(socialIcons)
+
+})
+
+*/
