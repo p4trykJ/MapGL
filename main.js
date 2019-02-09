57f46cefc05ba884d9d99fbe6d58dcd82913e1c6
Patryk <patryk.jezierski@gis-support.pl>
2019-02-09 15:24:49 +0100

    Fix - zakomentowanie funkcji toggleWathershade

--- main.js
+++ main.js
@@ -335,33 +335,33 @@ $(document).ready(toogleHillshade = function () {
   }
 })
 
-$(document).ready(toogleWatershade = function () {
-  $("#watershade__button")[0].onclick = function () {
-    if(map.getLayer("10m-bathymetry-81bsvj") == undefined) {
-      map.addLayer({
-        "id": "10m-bathymetry-81bsvj",
-        "type": "fill",
-        "source": "10m-bathymetry-81bsvj",
-        "source-layer": "10m-bathymetry-81bsvj",
-        "layout": {},
-        "paint": {
-          "fill-outline-color": "hsla(337, 82%, 62%, 0)",
-          "fill-color": ["interpolate",
-            ["cubic-bezier",
-              0, 0.5,
-              1, 0.5],
-            ["get", "DEPTH"],
-            200, "#78bced",
-            9000, "#15659f"
-          ]
-        }
-      });
-    }
-    else {
-      map.removeLayer("10m-bathymetry-81bsvj")
-    }
-  }
-})
+// $(document).ready(toogleWatershade = function () {
+//   $("#watershade__button")[0].onclick = function () {
+//     if(map.getLayer("10m-bathymetry-81bsvj") == undefined) {
+//       map.addLayer({
+//         "id": "10m-bathymetry-81bsvj",
+//         "type": "fill",
+//         "source": "10m-bathymetry-81bsvj",
+//         "source-layer": "10m-bathymetry-81bsvj",
+//         "layout": {},
+//         "paint": {
+//           "fill-outline-color": "hsla(337, 82%, 62%, 0)",
+//           "fill-color": ["interpolate",
+//             ["cubic-bezier",
+//               0, 0.5,
+//               1, 0.5],
+//             ["get", "DEPTH"],
+//             200, "#78bced",
+//             9000, "#15659f"
+//           ]
+//         }
+//       });
+//     }
+//     else {
+//       map.removeLayer("10m-bathymetry-81bsvj")
+//     }
+//   }
+// })
 
 /*
 $(document).ready(createSocialIcons = function() {
